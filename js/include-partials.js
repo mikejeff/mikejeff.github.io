71cc14c8165d6073f5ccabf8f2b1b0f63da091d6
Mike Jeffery <mikejeff.dev@gmail.com>
2026-05-04 16:03:36 -0400

    format code files

--- js/include-partials.js
+++ js/include-partials.js
@@ -1,19 +1,18 @@
-  async function includePartials() {
-    const includes = document.querySelectorAll('[data-include]');
+async function includePartials() {
+  const includes = document.querySelectorAll("[data-include]");
 
-    for (const el of includes) {
-      const path = el.getAttribute('data-include');
+  for (const el of includes) {
+    const path = el.getAttribute("data-include");
 
-      try {
-        const response = await fetch(path);
-        if (!response.ok) throw new Error(`Failed to load ${path}`);
+    try {
+      const response = await fetch(path);
+      if (!response.ok) throw new Error(`Failed to load ${path}`);
 
-        el.innerHTML = await response.text();
-      } catch (error) {
-        console.error(error);
-      }
+      el.innerHTML = await response.text();
+    } catch (error) {
+      console.error(error);
     }
-    
   }
+}
 
-  includePartials();
+includePartials();
